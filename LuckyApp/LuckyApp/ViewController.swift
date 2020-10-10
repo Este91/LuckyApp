a3451946f356cf77d51307fd82b37f73d165cd8b
Esteban <esteban.a.boffa@gmail.com>
2020-10-10 20:47:59 -0300

    Add tableView and navBar setup

--- LuckyApp/LuckyApp/ViewController.swift
+++ LuckyApp/LuckyApp/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        renderViews()
     }
+}
 
-
+// MARK: UI
+private extension ViewController {
+    func renderViews() {
+        setupNavBar()
+        setupTableView()
+    }
+    
+    func setupNavBar() {
+        navigationController?.navigationBar.barTintColor = .lightGray
+        title = "Lucky"
+    }
+    
+    func setupTableView() {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .none
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
 
