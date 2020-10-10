7a728b6629ad58b60dfc607917ca2e493d994591
Esteban <esteban.a.boffa@gmail.com>
2020-10-10 19:44:46 -0300

    Project creation

---
+++ LuckyApp/LuckyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LuckyApp
+//
+//  Created by Camila Perez Tellado on 10/10/2020.
+//  Copyright © 2020 EstebanBoffa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
