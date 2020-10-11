0168c6e32a275b88861d62e7d61d89164a1614e8
Esteban <esteban.a.boffa@gmail.com>
2020-10-11 02:02:37 -0300

    Add imageView extension

--- LuckyApp/LuckyApp/Extensions/UIImageView+RemoteImage.swift
+++ LuckyApp/LuckyApp/Extensions/UIImageView+RemoteImage.swift
@@ -2,8 +2,28 @@
 //  UIImageView+RemoteImage.swift
 //  LuckyApp
 //
-//  Created by Camila Perez Tellado on 11/10/2020.
+//  Created by Esteban Boffa on 11/10/2020.
 //  Copyright © 2020 EstebanBoffa. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+extension UIImageView {
+    func setRemoteImage(imageUrl: String, success:((UIImage)->Void)? = nil) {
+        NetworkLayer.request(imageUrl: imageUrl) { [weak self] (image) in
+            guard let self = self else { return }
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                UIView.transition(with: self, duration: 0.3, options: .transitionCrossDissolve, animations: { [weak self] in
+                        guard let self = self else { return }
+                        self.backgroundColor = .clear
+                        self.layer.cornerRadius = 0
+                        self.image = image
+                    }, completion: { _ in
+                        success?(image)
+                })
+            }
+        }
+    }
+}
