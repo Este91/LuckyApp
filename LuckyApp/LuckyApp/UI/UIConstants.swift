e9b89265914550fbd648dbc20ce23158439ceb9d
Esteban <esteban.a.boffa@gmail.com>
2020-10-11 02:24:45 -0300

    Add UIConstants

---
+++ LuckyApp/LuckyApp/UI/UIConstants.swift
@@ -0,0 +1,25 @@
+//
+//  UIConstants.swift
+//  LuckyApp
+//
+//  Created by Esteban Boffa on 11/10/2020.
+//  Copyright © 2020 EstebanBoffa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct UI {
+    
+    struct Margin {
+        static let XXXS_MARGIN: CGFloat = 2
+        static let XXS_MARGIN: CGFloat = 8
+        static let XS_MARGIN: CGFloat = 12
+        static let S_MARGIN: CGFloat = 16
+        static let M_MARGIN: CGFloat = 20
+        static let L_MARGIN: CGFloat = 24
+        static let XL_MARGIN: CGFloat = 32
+        static let XXL_MARGIN: CGFloat = 40
+    }
+    
+}
