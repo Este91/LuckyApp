8eb684b1967f0486fca92aa457a9e833e1ea9a2a
Esteban <esteban.a.boffa@gmail.com>
2020-10-11 14:36:40 -0300

    Add OfferDetail models

--- LuckyApp/LuckyApp/Model/OfferDetail.swift
+++ LuckyApp/LuckyApp/Model/OfferDetail.swift
@@ -2,8 +2,21 @@
 //  OfferDetail.swift
 //  LuckyApp
 //
-//  Created by Camila Perez Tellado on 11/10/2020.
+//  Created by Esteban Boffa on 11/10/2020.
 //  Copyright © 2020 EstebanBoffa. All rights reserved.
 //
 
 import Foundation
+
+struct OfferDetail: Codable {
+    let id: Int
+    let imageUrl: String
+    let brand: String
+    let title: String
+    let tags: String?
+    let favoriteCount: Int
+    let description: String
+    let price: Price
+    let expiration: String
+    let redemptionsCap: String
+}
--- LuckyApp/LuckyApp/Model/Price.swift
+++ LuckyApp/LuckyApp/Model/Price.swift
@@ -2,8 +2,13 @@
 //  Price.swift
 //  LuckyApp
 //
-//  Created by Camila Perez Tellado on 11/10/2020.
+//  Created by Esteban Boffa on 11/10/2020.
 //  Copyright © 2020 EstebanBoffa. All rights reserved.
 //
 
 import Foundation
+
+struct Price: Codable {
+    let old: String
+    let new: String
+}
